a5f9670e72a43e0007065e1a4b48a66b60d3c6ea
Oyasumisy <oyasumi2819@gmail.com>
2024-10-12 08:39:37 +0300

    Update practical4z1v1.java

--- practical4z1v1.java
+++ practical4z1v1.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class practical4z1v1 {
+    //Завдання 1; Варіант 1
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введіть число від 1 до 7, щоб дізнатися день тижня: ");
